d68edd2f022646b708356a6425c36c81aa5f7e08
zhangbin <zhang.bin@intellif.com>
2017-05-26 14:42:19 +0800

    modify String, add inifile

--- String.cpp
+++ String.cpp
@@ -18,6 +18,7 @@ std::vector<string> String::split(string str, string separator)
 	}
 	return result;
 }
+
 WCHAR *String::mbcsToUnicode(const char *zFilename)
 {
 	int nByte;
@@ -65,6 +66,7 @@ std::string String::encode(const std::string to_encode)
 	}
 	return res;
 }
+
 std::string String::decode(const std::string to_decode)
 {
 	std::string key = "4962873";
@@ -74,4 +76,107 @@ std::string String::decode(const std::string to_decode)
 		if (res[j] < 32) res[j] += 90;
 	}
 	return res;
+}
+
+void String::trimleft(string &str,char c/*=' '*/)
+{
+	//trim head
+    size_t len = str.length();
+
+	int i = 0;
+	while(str[i] == c && str[i] != '\0'){
+		i++;
+	}
+	if(i != 0){
+		str = string(str,i,len-i);
+	}
+}
+
+void String::trimright(string &str,char c/*=' '*/)
+{
+	//trim tail
+	int i = 0;
+    int len = (int)str.length();
+		
+	
+	for(i = len - 1; i >= 0; --i ){
+		if(str[i] != c){
+			break;
+		}
+	}
+
+	str = string(str,0,i+1);
+}
+
+void String::trim(string &str)
+{
+	//trim head
+    int len = (int)str.length();
+
+	int i   = 0;
+
+	while(isspace(str[i]) && str[i] != '\0'){
+		i++;
+	}
+	if(i != 0){
+		str = string(str,i,len-i);
+	}
+
+	//trim tail
+	len = (int)str.length();
+
+	for(i = len - 1; i >= 0; --i ){
+		if(!isspace(str[i])){
+			break;
+		}
+	}
+	str = string(str,0,i+1);
+}
+
+int String::DecodeRemotFileCurl(const std::string & rStrRemotUrl, cv::Mat & decodeMat)
+{
+	std::string strRevData;
+
+	CURL * pCurlHand = curl_easy_init();
+
+	if (pCurlHand == NULL)
+	{
+		return -1;
+	}
+	curl_easy_setopt(pCurlHand, CURLOPT_URL, rStrRemotUrl.c_str());
+	curl_easy_setopt(pCurlHand, CURLOPT_FOLLOWLOCATION, 1);
+	curl_easy_setopt(pCurlHand, CURLOPT_WRITEDATA, &strRevData);
+	curl_easy_setopt(pCurlHand, CURLOPT_WRITEFUNCTION, Write2String);
+	curl_easy_setopt(pCurlHand, CURLOPT_NOSIGNAL, 1L);
+	curl_easy_setopt(pCurlHand, CURLOPT_CONNECTTIMEOUT, 4);
+	curl_easy_setopt(pCurlHand, CURLOPT_TIMEOUT, 30);
+
+	CURLcode dwCurlRet = curl_easy_perform(pCurlHand);
+	if (dwCurlRet != CURLE_OK)
+	{
+		curl_easy_cleanup(pCurlHand);
+		return -1;
+	}
+
+	cv::Mat Bitstream(cv::Size((int)strRevData.size(), 1),
+		CV_8U,
+		(unsigned char *)strRevData.c_str());
+
+	decodeMat = cv::imdecode(Bitstream, CV_LOAD_IMAGE_COLOR);
+	curl_easy_cleanup(pCurlHand);
+	return 0;
+}
+
+int String::Write2String(char *data, size_t size, size_t nmemb, string * writerData)
+{
+	unsigned long sizes = size * nmemb;
+
+	if (writerData == NULL)
+	{
+		return 0;
+	}
+
+	writerData->append(data, sizes);
+
+	return sizes;
 }
--- String.h
+++ String.h
@@ -1,6 +1,7 @@
 #ifndef STRING_H
 #define STRING_H
 #include <iostream>
+#include <string>
 using namespace std;
 class String
 {
@@ -10,5 +11,10 @@ public:
 	char *unicodeToUtf8(const WCHAR *zWideFilename);
 	std::string encode(const std::string to_encode);
 	std::string decode(const std::string to_decode);
+	void trimleft(string &str,char c=' ');
+	void trimright(string &str,char c=' ');
+	void trim(string &str);
+	int DecodeRemotFileCurl(const std::string & rStrRemotUrl, cv::Mat & decodeMat);
+	int Write2String(char *data, size_t size, size_t nmemb, string * writerData);
 };
 #endif
